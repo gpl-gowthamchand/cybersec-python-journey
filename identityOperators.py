566d8d935fbbdedf47ebf8338933702a13375ac0
gpl-gowthamchand <gpl.gowthamchand@gmail.com>
2025-10-15 19:02:45 +0530

    identity operators

---
+++ identityOperators.py
@@ -0,0 +1,11 @@
+# Identity operators
+
+a = 10 
+b = 10 
+
+print( a is b )     # is - returns True if both variables are pointing to a same object
+
+print( a is not b)  # is not - returns True if both variables are not pointing to same object 
+
+
+# I have small confusion with this Idententy operators, if this examples are not correct please let me know or free to modify and contribute.. 
