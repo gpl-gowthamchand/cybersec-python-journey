3d63d203b78607aa78443fa50314f6b1c5d27d3d
gpl-gowthamchand <gpl.gowthamchand@gmail.com>
2025-10-15 18:54:05 +0530

    membership operators

---
+++ membershipOperators.py
@@ -0,0 +1,18 @@
+# membership operators 
+
+fruits = [ "grapes", "mango", "sapota"]
+
+
+# in - returns true if value present in that sequence
+is_Grapes_inList = "grapes" in fruits
+print(is_Grapes_inList)
+
+is_Apple_inList = "apple" in fruits
+print(is_Apple_inList)
+
+# not in - returns true if value not present in that sequence
+is_Grapes_inList = "grapes" not in fruits
+print(is_Grapes_inList)
+
+is_Apple_inList = "apple" not in fruits
+print(is_Apple_inList)
