6fa05cb487c248a5b5bdb847d0bc9f841801e275
gpl-gowthamchand <gpl.gowthamchand@gmail.com>
2025-10-15 12:48:36 +0530

    ascii and chars

---
+++ ascii-val.py
@@ -0,0 +1,13 @@
+# ord() to find the ascii of char
+
+char_a = 'A'
+ascii_a = ord(char_a)
+print("ascii val of A is ", ascii_a)
+
+
+
+# chr() function to find the char of ascii val
+
+ascii_b = 66 
+char_b = chr(ascii_b)
+print("char of given ascii 66 is ", char_b)
