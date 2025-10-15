271cb6845e59347b7841a228b7448f54538526a3
gpl-gowthamchand <gpl.gowthamchand@gmail.com>
2025-10-15 18:30:50 +0530

    comparison operators

---
+++ comparisonOperators.py
@@ -0,0 +1,36 @@
+## Comparison operators 
+
+# == - equal to 
+a = 2
+b = 2
+print(a == b)
+
+# != - not equal to
+
+a = 7
+b = 6 
+print( a != b )
+
+# > - greater than
+
+a = 7
+b = 6 
+print( a > b )
+
+# < - less than
+
+a = 7
+b = 6 
+print( a < b ) 
+
+# >= - greater than or equal to
+
+a = 7
+b = 6 
+print( a >= b )
+
+# <= - less than or equal to
+
+a = 7
+b = 6 
+print( a <= b )
