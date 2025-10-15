f23b2baa6f979368023eeef991800d733493ca77
gpl-gowthamchand <gpl.gowthamchand@gmail.com>
2025-10-15 18:02:58 +0530

    arithmetic operators

---
+++ arithmeticOperators.py
@@ -0,0 +1,37 @@
+## arithmetic operators
+
+# add
+num1 = 2
+num2 = 8
+sum = num1 + num2
+print(sum)
+
+# subtract
+a= 5
+b = 3
+print(a - b)
+
+# multifly
+x = 6
+y = 8
+z = x*y
+print(z)
+
+# division  ( returns quotient but in float value)
+p = 10
+g = 2
+print(p / g)
+
+# floor division ( returns quotient but in integer value)
+l = 10
+k = 2
+print(l // k)
+
+# modulo (returns remainder after division)
+n = 10
+m = 2
+print(n % m)
+
+# Exponential (power)
+i = 2
+print(i**3)
