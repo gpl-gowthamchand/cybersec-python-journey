adbdeda43a71ffbd41361ba06f716c12dea1579a
gpl-gowthamchand <gpl.gowthamchand@gmail.com>
2025-10-15 10:37:38 +0530

    Initial commit

---
+++ dayTwo-typeCast.py
@@ -0,0 +1,19 @@
+a = 5
+print(a) 
+print (type(a)) 
+
+b = 6.5
+print(b) 
+print (type(b))
+
+c = a+b 
+print(c)
+print (type(c))
+
+x = "100"
+print(x)
+print (type(x))
+
+y = int(x)
+print(y)
+print (type(y))
---
+++ handsOnExamples-typecast.py
@@ -0,0 +1,28 @@
+num = 5 + 2.0 
+print (num)
+print (type(num))
+
+# type casting to int 
+num_int = int(5.7)
+print (num_int)
+print (type(num_int))
+
+# type casting to float
+num_float = float("3.14")
+print (num_float)
+print (type(num_float))
+
+# type casting to string
+message = str(42)
+print (message)
+print (type(message))
+
+# type casting to boolean 
+is_true = bool(1)
+is_false = bool(0)
+print (is_true)
+print (type(is_true))
+print (is_false)
+print (type(is_false))
+
+
