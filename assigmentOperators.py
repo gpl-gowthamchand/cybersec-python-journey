2dfe1b272f5e77fb75a16edd62f0d58a583cebf2
gpl-gowthamchand <gpl.gowthamchand@gmail.com>
2025-10-15 18:16:21 +0530

    assignment operators

---
+++ assigmentOperators.py
@@ -0,0 +1,41 @@
+## Assignment Operators 
+
+# assign 
+name = "gowtham"
+num = 18
+
+# add and assign 
+a = 10
+a += 7  # a = a+7 i.e a = 10+7
+print(a)
+
+# subtract and assign 
+a = 9
+a -= 3
+print (a)
+
+# multifly and assign 
+a = 5 
+a *= 3
+print(a)
+
+# devide and assign
+a = 8
+a /= 4
+print(a)
+
+# floor-devision and assign
+a = 8
+a //= 4
+print(a)
+
+# modulo and assign
+a = 8
+a %= 4
+print(a)
+
+# Exponential and assign
+a = 5
+a **= 3
+print(a)
+
