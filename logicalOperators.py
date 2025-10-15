e6b20961d31ca042abbd78573813a52c56acdedb
gpl-gowthamchand <gpl.gowthamchand@gmail.com>
2025-10-15 18:39:28 +0530

    logical operators

---
+++ logicalOperators.py
@@ -0,0 +1,12 @@
+# Logical operators 
+
+
+x = True
+y = False
+
+print ( x and y )  # and logical operator returns True if both are true
+
+print ( x or y )   # or logical operator returns True if one of them is true
+
+print ( not x )    # reverses the result; returns True if it false. returns False if it is true
+print ( not y )
