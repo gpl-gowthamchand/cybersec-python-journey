b170fc7e4c4bb7ac6a973ea07d057d53e87dd2a6
gpl-gowthamchand <gpl.gowthamchand@gmail.com>
2025-10-15 13:08:48 +0530

    basic datatypes

---
+++ basic-Datatypes.py
@@ -0,0 +1,45 @@
+## text -- string - str
+
+name= "Gowtham"
+msg = 'hello'
+para = '''this is a 
+multi-line string '''
+
+
+## numeric 
+#int
+
+num = 5
+
+# float
+deci = 5.7
+
+# complex 
+comp_num = 3+4j
+
+## Boolean -- bool
+
+
+## sequence 
+
+# list 
+list_of_items = [1, "gowtham", 'a', 1.2]
+
+# tuple
+fruits = ("grapes", "grapes type2", "grapes type3")
+
+# range -- commonly used in loops 
+r = range(5)
+r = range(2, 10, 2)
+
+
+## maping datatypes 
+# dict 
+student= { "name": "gowtham", "age": 21 }
+
+## Sets 
+# set
+a= {1,2,3}
+
+# frozenset
+b = frozenset({1,2,3})
--- test.py
+++
@@ -1,8 +0,0 @@
-a = 10
-b = float(a)
-
-print (a)
-print(type(a))
-
-print(b)
-print(type(b))
