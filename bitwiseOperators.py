a80ef9ec9caf6ea1cc17e4eff8a24dd4650a0c2c
gpl-gowthamchand <gpl.gowthamchand@gmail.com>
2025-10-16 07:34:45 +0530

    bitwise operators

---
+++ bitwiseOperators.py
@@ -0,0 +1,14 @@
+## bitwise operators 
+
+print(2 & 3)  # & - bitwise and -sets bits to 1 if both bits are 1
+
+print(2 | 3)  # | - bitwise or -sets bits to 1 if atleast one of the bits is 1
+
+print(2 ^ 3)  # ^ - bitwise xor -sets bits to 1 if only one bit is 1
+
+print( ~ 2)   # ~ - bitwise not -inverts the bits, changing 1to0 and 0to1
+
+print(2 << 2) # << -bitwise left shift -shifts the bits to the left by a specified no. of positions
+
+print(2 >> 1) # >> -bitwise right shift -shifts the bits to the right by a specified no. of positions
+
