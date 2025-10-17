d5ae71ff4baae647ecd3b750e95cd03b78cc2b63
gpl-gowthamchand <gpl.gowthamchand@gmail.com>
2025-10-17 22:16:26 +0530

    added 9,10,11

---
+++ practise-examples-set1.py/example10.py
@@ -0,0 +1,11 @@
+# input: Yes (or yes, YES, yEs, etc)
+# output: You entered:Yes
+
+
+a = input("Enter input: ")
+print(f"You entered:{a}")
+
+
+
+
+## i have to work on this later
---
+++ practise-examples-set1.py/example11.py
@@ -0,0 +1,5 @@
+# input: "Alice",25
+# output: Name:Alice,Age:25 years
+
+name, age = input("Enter name and age seperated by comma: ").split(",")
+print(f"Name:{name},Age:{25} years")
