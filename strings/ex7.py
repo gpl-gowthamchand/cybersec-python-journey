5620dadd446eea10f1165a34dd10a131f3f46500
gpl-gowthamchand <gpl.gowthamchand@gmail.com>
2025-10-29 17:45:41 +0530

    ex7 slicing

---
+++ strings/ex7.py
@@ -0,0 +1,23 @@
+''' 
+  P   y   t   h   o   n   --- size = 6
+  0   1   2   3   4   5   
+ -6  -5  -4  -3  -2  -1
+
+string[ start : end : step ]
+
+  start- inclusive 
+  end  - exclusive 
+
+'''
+
+s = "Python"
+print(s[1])
+print(s[-1])
+print(s[1:3])
+print(s[1:-1])
+print(s[:3]) 
+print(s[2:])
+print(s[:-1])
+print(s[::2])
+print(s[1::2])
+print(s[::-1])
