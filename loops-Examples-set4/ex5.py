1cd30b930d352051f489de4bdac9ce55e296afdc
gpl-gowthamchand <gpl.gowthamchand@gmail.com>
2025-11-01 13:57:27 +0530

    factorial ex update

--- loops-Examples-set4/ex5.py
+++ loops-Examples-set4/ex5.py
@@ -1,16 +1,18 @@
 # Calculate the factorial of a number using a while loop. 
 
-num = 3
-fact = 1
+num = int(input("Enter a number: "))
+numToIterate = num
+factorial = 1 
 
-if num == 1:
-    fact = 1
+if numToIterate == 0:
+    factorial = 1
+    print(f"the factorial of {num} is {factorial}")
+
 else:
-    while(num >= 2):
-        fact = fact * num
-        num -=1
-
-print(fact)
+    while numToIterate > 0:
+        factorial = factorial*numToIterate 
+        numToIterate = numToIterate-1
+    print(f"the factorial of {num} is {factorial}")
 
 
 
