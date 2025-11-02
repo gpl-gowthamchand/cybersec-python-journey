b8ce6a777def28f8c277dba3ac67f647ded799e7
gpl-gowthamchand <gpl.gowthamchand@gmail.com>
2025-11-02 14:30:53 +0530

    ex5

---
+++ lists/ex5.py
@@ -0,0 +1,25 @@
+# Reverse a list using slicing.
+
+numbers = [1, 2, 3, 4, 5]
+
+reversed_list = numbers[::-1]
+
+print(reversed_list)
+
+
+'''
+Using reversed() function
+
+numbers = [1, 2, 3, 4, 5]
+reversed_list = list(reversed(numbers))
+print(reversed_list)
+
+
+
+Using .reverse() method (changes the list in place)
+
+numbers = [1, 2, 3, 4, 5]
+numbers.reverse()
+print(numbers)
+
+'''
