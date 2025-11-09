63322843f470f034b2b38f660140b4acde43654f
gpl-gowthamchand <gpl.gowthamchand@gmail.com>
2025-11-09 16:47:43 +0530

    ex6

---
+++ sets/ex6.py
@@ -0,0 +1,11 @@
+'''
+Disjoint Sets
+Check if these two sets are disjoint (no common elements):
+set1 = {1, 2, 3}
+set2 = {4, 5, 6}
+'''
+
+set1 = {1, 2, 3}
+set2 = {4, 5, 6}
+
+print(set1.isdisjoint(set2))  # True
