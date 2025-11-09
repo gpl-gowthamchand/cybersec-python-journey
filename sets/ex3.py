3be972a6f91bacd8c5eb4f18f68e72e0aa5c2908
gpl-gowthamchand <gpl.gowthamchand@gmail.com>
2025-11-09 16:33:27 +0530

    ex3

---
+++ sets/ex3.py
@@ -0,0 +1,9 @@
+'''
+Convert List to Set
+Given: nums = [1, 2, 2, 3, 4, 4, 5]
+Convert it into a set to remove duplicates. '''
+
+
+nums = [1, 2, 2, 3, 4, 4, 5] 
+set_numbers = set(nums) 
+print(set_numbers)        # {1, 2, 3, 4, 5} - duplicates are removed automatically
