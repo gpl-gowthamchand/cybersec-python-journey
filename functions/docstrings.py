5dbc04e494c4a341b2918af0b06cf195474fd0ee
gpl-gowthamchand <gpl.gowthamchand@gmail.com>
2025-11-14 22:23:35 +0530

    functions

---
+++ functions/docstrings.py
@@ -0,0 +1,9 @@
+## Docstrings (function documentation)
+
+''' You can document what a function does using triple quotes '''
+
+def addition_of_two(a, b):
+    """This function adds two numbers"""
+    return a+b 
+
+print(addition_of_two.__doc__)
---
+++ functions/lambdaFunction.py
@@ -0,0 +1,11 @@
+# Lambda Functions 
+''' lambda function is short, anonymous function defined using lambda keyword.
+It can take any number of arguments but restricted to a single expression, the result of which 
+is automatically returned. 
+
+var_name = lambda arg : expression 
+
+'''
+
+cube_of_num = lambda number : number**3 
+print(cube_of_num(3))
