a1950eafb01e0790d4d5988931f8213e2a8afa4e
gpl-gowthamchand <gpl.gowthamchand@gmail.com>
2025-11-18 21:44:52 +0530

    ex10.py

---
+++ functions/ex10.py
@@ -0,0 +1,9 @@
+'''
+Reverse a String
+Write a function reverse_string(s) that returns the reversed string using slicing.
+'''
+
+def reverse_string(s):
+    return s[::-1]
+
+print(reverse_string("Gowtham"))
